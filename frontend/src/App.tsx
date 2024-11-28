b4351dcbc8bdbe92aba62d05ec46f260690b7c8d
Tim Van Lerberg <tim@brightsidedeveloper.com>
2024-11-27 21:34:39 -0600

    Node Module

---
+++ frontend/src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="text-7xl text-red-500 flex items-center justify-center h-dvh">
+      <span>GO Served This!</span>
+    </div>
+  )
+}
